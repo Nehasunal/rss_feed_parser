52bcdc67bdbaefc57c31b906a3e8bf9e452a438d
neha.sunal <nehasunal04@gmail.com>
2024-03-31 14:16:19 +0530

    response fix

--- controller/controller.js
+++ controller/controller.js
@@ -12,45 +12,61 @@ exports.getRssFeed = async function(req, res) {
 
         // Parse XML using xml2js
         parseString(xml, (err, result) => {
-          if (err) {
-            throw err;
-          }
-          
-          // Extract relevant data from the parsed XML
-          const items = result.rss.channel[0].item;
-          
-          // Apply dynamic limit to the number of items
-          let limit = parseInt(req.query.limit);
-          if (!limit || isNaN(limit)) {
-            // If limit is not provided or invalid, default to 10
-            limit = 10;
-          }
-    
-          // Filter items based on search query
-          let filteredItems = items;
-          const searchQuery = req.query.query;
-          if (searchQuery) {
-            const queryRegex = new RegExp(searchQuery, 'i'); // Case-insensitive regex
-            filteredItems = items.filter(item => {
-              return queryRegex.test(item.title[0]) || queryRegex.test(item.description[0]);
-            });
-          }
+            if (err) {
+                throw err;
+            }
+            
+            // Extract relevant data from the parsed XML
+            const items = result.rss.channel[0].item;
+                        
+            let filteredItems = items;
+            // Apply dynamic limit to the number of items
+            let limit = parseInt(req.query.limit);
+            if (!limit || isNaN(limit)) {
+                // If limit is not provided or invalid, default to 10
+                limit = 10;
+            }
+        
+            // Filter items based on search query
+            const searchQuery = req.query.search;
+            if (searchQuery) {
+                const queryRegex = new RegExp(searchQuery, 'i'); // Case-insensitive regex
+                filteredItems = items.filter(item => {
+                return queryRegex.test(item.title[0]) || queryRegex.test(item.description[0]);
+                });
+            }
 
-          if(req.query.startDate && req.query.endDate){
-          // Filter items based on published date range
-          const startDate = new Date(req.query.startDate);
-          const endDate = new Date(req.query.endDate);
-          filteredItems = filteredItems.filter(item => {
-              const publishedDate = new Date(item.pubDate[0]);
-              return publishedDate >= startDate && publishedDate <= endDate;
-          });
-          }
+          // Filter items based on published date range if startDate is provided
+            if (req.query.startDate) { //mm-dd-yyyy
+                const startDate = new Date(req.query.startDate);
+                filteredItems = filteredItems.filter(item => {
+                    const publishedDate = new Date(item.pubDate);
+                    return publishedDate >= startDate;
+                });
+            }
+
+            // Filter items based on published date range if endDate is provided
+            if (req.query.endDate) {
+                const endDate = new Date(req.query.endDate);
+                filteredItems = filteredItems.filter(item => {
+                    const publishedDate = new Date(item.pubDate);
+                    return publishedDate <= endDate;
+                });
+            }
     
-          // Apply limit to the filtered items
-          const limitedItems = filteredItems.slice(0, limit);
-    
-          // Send parsed data as JSON response
-          res.json(limitedItems);
+            // Apply limit to the filtered items
+            const limitedItems = filteredItems.slice(0, limit);
+        
+            const formattedResponse = limitedItems.map(item => ({
+                title: item.title[0],
+                description: item.description[0],
+                link: item.link[0],
+                guid: item.guid[0],
+                pubDate: item.pubDate[0]
+            }));
+
+            // Send the formatted response
+            res.json(formattedResponse);
         });
       } catch (error) {
         console.error('Error retrieving or parsing RSS feed:', error);
